6f4da38dc8fd1180aecd679b348458454d8adb6f
karim slama <karimslama917@gmail.com>
2023-08-04 16:29:41 +0300

    splash_screen

--- lib/jobizz/presentation/screens/splash_screen.dart
+++ lib/jobizz/presentation/screens/splash_screen.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:project/core/utils/theme/app_colors/app_colors.dart';
+import 'package:project/jobizz/presentation/screens/on_boarding_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.darkGreyBlue,
-      body: Center(
-        child: Image.asset('assets/images/logo.png'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xff356899),
+              Color(0xff1A334C),
+            ],
+          ),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Image.asset('assets/images/logo.png'),
+        ),
       ),
+    );
+  }
+
+  Future navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 4), () {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome().then(
+      (value) => Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const OnBoardingScreen(),
+          ),
+          (route) => false),
     );
   }
 }
