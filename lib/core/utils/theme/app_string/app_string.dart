8c3bb2d4af8f3ed4023fafd495e27a0b74289b55
masterdee121212 <primemerchantapp@gmail.com>
2024-10-09 22:47:14 +0800

    Update app_string.dart

--- lib/core/utils/theme/app_string/app_string.dart
+++ lib/core/utils/theme/app_string/app_string.dart
@@ -4,7 +4,7 @@ class AppString {
       'Figure out your top five priorities whether it is company culture, salary.';
   static const browseJobsList = 'Browse jobs list';
   static const ourJobListIncludeSeveralIndustriesSoYouCanFindTheBestJobForYou =
-      'Our job list include several  industries, so you can find the best job for you.';
+      'Our job list includes several industries, so you can find the best job for you.';
   static const applyToBestJobs = 'Apply to best jobs';
   static const youCanApplyToYourDesirableJobsVeryQuicklyAndEasilyWithEase =
       'You can apply to your desirable jobs very quickly and easily with ease.';
@@ -14,7 +14,7 @@ class AppString {
   static const searchYourDreamJobFastAndEase =
       'Search your dream job fast and ease';
   static const figureOutYourTopFivePrioritiesWhetherItIsCompanyCultureSalaryOrASpecificJobPosition =
-      'Figure out your top five priorities whether it is company culture, salary or a specific job position.';
+      'Figure out your top five priorities whether it is company culture, salary, or a specific job position.';
   static const makeYourDreamCareerWithJob = 'Make your dream career with job';
   static const weHelpYouFindYourDreamJobAccordingToYourSkillsetLocationPreferenceToBuildYourCareer =
       'We help you find your dream job according to your skillset, location & preference to build your career.';
@@ -38,28 +38,30 @@ class AppString {
   static const forgotPassword = 'Forgot Password?';
   static const haveNotAnAccount = 'Haven\'t an account?';
   static const enterYourEmailOrPhoneNumberWeWillSendYouVerificationCode =
-      'Enter your email or phone number, we will send you verification code';
+      'Enter your email or phone number, we will send you a verification code';
   static const sendCode = 'Send Code';
   static const verifyCode = 'Verify Code';
   static const enterYourVerificationCodeFromYourEmailOrPhoneNumberThatWeHaveSent =
       'Enter your verification code from your email or phone number that we’ve sent';
-  static const mustNotBeEmpty = 'must not be empty';
+  static const mustNotBeEmpty = 'Must not be empty';
   static const verify = 'Verify';
   static const resetPassword = 'Reset Password';
-  static const enterYourNewPasswordAndConfirmTheNewPasswordToResetPassword = 'Enter your new password and confirm the new password to reset password';
+  static const enterYourNewPasswordAndConfirmTheNewPasswordToResetPassword = 
+      'Enter your new password and confirm the new password to reset password';
   static const confirmation = 'Confirmation';
-  static const yourPasswordHasBeenChangedPleaseLoginWithYourNewPassword = 'Your password has been changed. Please log in with your new password.';
+  static const yourPasswordHasBeenChangedPleaseLoginWithYourNewPassword = 
+      'Your password has been changed. Please log in with your new password.';
   static const whatTypeOfJobYouAreLookingFor = 'What type of job you’re looking for?';
   static const designer = 'Designer';
   static const developer = 'Developer';
-  static const administrative   = 'Administrative  ';
-  static const marketing   = 'Marketing  ';
-  static const management   = 'Management  ';
-  static const others   = 'Others  ';
-  static const fullTime   = 'Full-Time  ';
-  static const partTime   = 'Part-Time  ';
-  static const contract   = 'Contract  ';
-  static const internship   = 'Internship  ';
-  static const freelance   = 'Freelance  ';
-  static const save   = 'Save  ';
+  static const administrative = 'Administrative';
+  static const marketing = 'Marketing';
+  static const management = 'Management';
+  static const others = 'Others';
+  static const fullTime = 'Full-Time';
+  static const partTime = 'Part-Time';
+  static const contract = 'Contract';
+  static const internship = 'Internship';
+  static const freelance = 'Freelance';
+  static const save = 'Save';
 }
