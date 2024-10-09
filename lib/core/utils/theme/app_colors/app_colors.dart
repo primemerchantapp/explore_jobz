8b1fc0b18742b396a144dae0c09153dc2da0a626
masterdee121212 <primemerchantapp@gmail.com>
2024-10-09 22:45:55 +0800

    Update app_colors.dart

--- lib/core/utils/theme/app_colors/app_colors.dart
+++ lib/core/utils/theme/app_colors/app_colors.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const darkGreyBlue = Color(0xff224261);
-  static const lightGreyBlue = Color(0xff2C557D);
-  static const black = Color(0xff0D0D26);
-  static const white = Color(0xffFFFFFF);
-  static const grey = Color(0xff95969D);
-  static const silver = Color(0xffE4E5E7);
-  static const lightGrey = Color(0xffAFB0B6);
+  // Primary Colors
+  static const darkGreyBlue = Color(0xff224261);  // Primary dark blue color
+  static const lightGreyBlue = Color(0xff2C557D); // Lighter variant of the primary color
+
+  // Neutral Colors
+  static const black = Color(0xff0D0D26);         // Standard black for text and other dark areas
+  static const white = Color(0xffFFFFFF);         // Standard white for backgrounds or text
+  static const grey = Color(0xff95969D);          // Grey for secondary text
+  static const silver = Color(0xffE4E5E7);        // Light silver for borders or secondary elements
+  static const lightGrey = Color(0xffAFB0B6);     // Lighter grey for less prominent elements or backgrounds
+
+  // You could also add accent colors if needed, like:
+  static const greenButton = Color(0xff6B8E23);   // Accent color for buttons
 }
